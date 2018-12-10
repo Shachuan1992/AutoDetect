08076eb03a6a59d54ff494d6de4ade947764ee9b
shachuan <sha@chinaunicom.cn>
2018-12-10 18:05:37 +0800

    整理成类的形式，加深印象

--- telnet.py
+++ telnet.py
@@ -1,21 +1,22 @@
 import telnetlib
 
-def write_command(instance,command):
-    finish = '\r'
-    instance.write(command.encode('ascii')+finish.encode('ascii'))
+class olt():
+    def __init__(self, IP=None,Device = None):
+        IP = IP
+        Device = Device
 
-def command_make(Device):
-    user = 'lyread'
-    password = 'read1234'
-    commands = []
-    if Device == 'MA5680T':
-        commands = ['MA5680T']
-    elif Device == 'MA5683T':
-        commands = ['MA5683T']
-    elif Device == 'C300':
-        commands = ['PON']
-    elif Device == 'C220':#命令不对
-        commands = ['C220']
-    else:
-        print("错误！！！没有这种设备，联系57591添加命令")
-    return commands
+    def write_command(instance,command):
+        finish = '\r'
+        instance.write(command.encode('ascii')+finish.encode('ascii'))
+
+    def command_make(Device):
+        user = 'lyjing'
+        password = 'lyjing!@#123'
+
+    def login(IP):
+        try:
+            tn = telnetlib.Telnet(host=IP, port=23, timeout=10)  # 尝试开启Telnet Socket
+        except ConnectionRefusedError:
+            print("错误！！！目标计算机连接超时，请检查链路是否正常！")
+        else:
+            print('Hello World')
