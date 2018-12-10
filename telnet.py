f4bf811d687346d9c1aed48dff6ac581533a8a4c
shac@chinaunicom.cn <Shachuan1992!>
2018-12-10 19:55:55 +0800

    逻辑初步构建

--- telnet.py
+++ telnet.py
@@ -1,21 +1,23 @@
 import telnetlib
 
-class olt():
+class telnet():
     def __init__(self, IP=None,Device = None):
         IP = IP
         Device = Device
 
-    def write_command(instance,command):
+    def write_command(self,instance,command):
         finish = '\r'
         instance.write(command.encode('ascii')+finish.encode('ascii'))
 
-    def command_make(Device):
+    def command_make(self,Device):
         user = 'lyjing'
         password = 'lyjing!@#123'
 
-    def login(IP):
+    def change(self,IP):
         try:
             tn = telnetlib.Telnet(host=IP, port=23, timeout=10)  # 尝试开启Telnet Socket
+            reply = str(tn.read_very_eager(),encoding='utf-8')
+            print(reply)
         except ConnectionRefusedError:
             print("错误！！！目标计算机连接超时，请检查链路是否正常！")
         else:
